741557213e642e712f1be69f82d68c9847d54582
Ujjwal Bhatt <ujjwalbhatt09@gmail.com>
2024-08-20 19:46:31 +0530

    designed the sign in component

--- frontend/src/components/Signin.jsx
+++ frontend/src/components/Signin.jsx
@@ -1,7 +1,63 @@
 import React from "react";
 
 const Signin = () => {
-  return <div>Signin</div>;
+  return (
+    <div className="flex items-center justify-center min-h-screen w-full bg-black">
+      <div className="bg-white rounded-lg shadow-md p-6 w-[21%]">
+        <h2 className="text-3xl font-bold mb-2 text-center">Sign In</h2>
+        <p className="text-gray-500 text-center mb-6">
+          Enter your credentials to access your account
+        </p>
+
+        <form>
+          <div className="mb-4">
+            <label
+              for="email"
+              className="block text-sm font-bold text-gray-700 mb-2"
+            >
+              Email
+            </label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              placeholder="johndoe@example.com"
+              className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-1 focus:ring-blue-500"
+            />
+          </div>
+
+          <div className="mb-6">
+            <label
+              for="password"
+              className="block text-sm font-bold text-gray-700 mb-2"
+            >
+              Password
+            </label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-1 focus:ring-blue-500"
+            />
+          </div>
+
+          <button
+            type="submit"
+            className="w-full bg-black text-white py-2 px-4 rounded-md hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
+          >
+            Sign In
+          </button>
+        </form>
+
+        <p className="mt-4 text-center text-sm">
+          Don't have an account?
+          <a href="#" className="ml-1 underline">
+            Sign Up
+          </a>
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Signin;
