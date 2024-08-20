c590c6257523138a44cf4e706f3b731ea4234b4b
Ujjwal Bhatt <ujjwalbhatt09@gmail.com>
2024-08-20 15:09:25 +0530

    added routing to the project

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,20 @@
+import { Route, Routes } from "react-router-dom";
+import Signup from "./components/Signup";
+import Signin from "./components/Signin";
+import Dashboard from "./components/Dashboard";
+import SendMoney from "./components/SendMoney";
+
 function App() {
-  return <div>Hello world</div>;
+  return (
+    <div>
+      <Routes>
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/send" element={<SendMoney />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
---
+++ frontend/src/components/SendMoney.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SendMoney = () => {
+  return <div>SendMoney</div>;
+};
+
+export default SendMoney;
