ddc4103ee200e00c5f57db2aab4c313cc40a83c6
Ujjwal Bhatt <ujjwalbhatt09@gmail.com>
2024-08-31 21:37:41 +0530

    minor change to backend

--- backend/index.js
+++ backend/index.js
@@ -3,10 +3,11 @@ const mainRouter = require("./routes/index");
 const app = express();
 const cors = require("cors");
 require("dotenv").config();
+const port = process.env.PORT || 8080;
 
 app.use(express.json());
 app.use(cors());
 
 app.use("/api/v1", mainRouter);
 
-app.listen(8080);
+app.listen(port);
