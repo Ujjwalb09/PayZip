bb8dcbc29083b45ad215eb9c73c0c665301376eb
Ujjwal Bhatt <ujjwalbhatt09@gmail.com>
2024-09-18 11:55:14 +0530

    cors error solved

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,15 @@ require("dotenv").config();
 const port = process.env.PORT || 8080;
 
 app.use(express.json());
-app.use(cors());
+
+app.use(
+  cors({
+    origin: "https://payzip.vercel.app", // Replace with your frontend URL
+    methods: ["GET", "POST", "PUT", "DELETE"], // Define allowed HTTP methods
+    allowedHeaders: ["Content-Type", "Authorization"], // Define allowed headers
+    credentials: true, // Enable this if requests include credentials like cookies
+  })
+);
 
 app.use("/api/v1", mainRouter);
 
