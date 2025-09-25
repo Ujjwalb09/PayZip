63c3fb5e718f292abba9196f8951485dab3a17eb
Ujjwal-zodix <Ujjwal.Bhatt@zodix.net>
2025-09-25 09:42:59 +0530

    Update mailSender.js

--- backend/utils/mailSender.js
+++ backend/utils/mailSender.js
@@ -21,7 +21,7 @@ const mailSender = async (email, title, body) => {
 
     // Send emails to users
     let info = await transporter.sendMail({
-      from: "Payments App",
+      from: `"Payments App" <${process.env.MAIL_USER}>`,
       to: email,
       subject: title,
       html: body,
